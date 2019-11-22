8d96b682d95eb6a627dc050400e3802f644099e8
david <davidndy@gmail.com>
2019-11-21 20:48:02 -0500

    agregar la funcion de editar Categoria y editar Evento

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -10,4 +10,14 @@ class Api::V1::CategoriesController < ApplicationController
       render json: "Hubo un error creando la categoria", status: 422
     end
   end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(nombre: params[:nombre])
+      head :ok
+    else
+      render json: "Hubo un error y la categoria no fue editada exitosamente", status: 422
+    end
+  end
 end
--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::EventsController < ApplicationController
 
   def index
     @events = params[:palabra] ? Event.buscar_por_palabra(palabra: params[:palabra]) : Event.all
-puts @events
+
     response = @events.map(&:construir_respuesta)
 
     render json: response.to_json, status: 200
@@ -13,8 +13,8 @@ puts @events
     @category = obtener_categoria(nombre_categoria: event_params[:categoria])
 
     if @category.events.create(
-      fecha: event_params[:fecha],
-      texto: event_params[:texto],
+      fecha:      event_params[:fecha],
+      texto:      event_params[:texto],
       url_imagen: event_params[:url_imagen]
     )
       head :ok
@@ -31,6 +31,25 @@ puts @events
     end
 
     render json: @events.map(&:construir_respuesta).to_json, status: 200
+  end
+
+  def update
+    @event = Event.find(event_params[:id])
+
+    @categoria = obtener_categoria(nombre_categoria: event_params[:categoria]) if event_params[:categoria]
+
+    if @event.update(
+      fecha:       event_params[:fecha],
+      texto:       event_params[:texto],
+      url_imagen:  event_params[:url_imagen],
+      category_id: @categoria.id || event.category.id
+      
+    )
+      head :ok
+    else
+      puts @event.errors.full_messages
+      render json: "Hubo un error y el evento no fue editado exitosamente", status: 422
+    end
   end
 
   private
